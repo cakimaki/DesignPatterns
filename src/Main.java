8d5c4a17004509cfa04ae56989ecf22082b27bd5
cakimaki <dani.tsakovski3@gmail.com>
2025-03-31 19:46:41 +0300

    Design patterns exercise/

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import creational.Singleton;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		
+		
+		System.out.println(Singleton.getInstance("data"));
+	}
+}
